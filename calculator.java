cf0aa05206bb5d9822f7ba21b9e732c24c806438
Mpho Dave <[mothibimphodavid@gmail.com]>
2022-01-17 09:19:32 +0200

    New Functions

--- calculator.java
+++ calculator.java
@@ -9,13 +9,15 @@ class Main {
     // create an object of Scanner class
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -");
-    operator = input.next().charAt(0);
+    
 
     // ask users to enter numbers
     System.out.println("Enter first number");
     number1 = input.nextInt();
+
+    // ask users to enter operator
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
 
     System.out.println("Enter second number");
     number2 = input.nextInt();
@@ -34,13 +36,39 @@ class Main {
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
 
-    
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
 
+      // performs division between numbers
+      case '/':
+       
+      if(number2==0) {
+        System.out.println("Please note that you cannot divide by 0");
+        
+        
+      }
+      else{
+        
+        result = number1 / number2;
+      System.out.println(number1 + " / " + number2 + " = " + result);
+
+      }
+      break;
+    
       default:
         System.out.println("Invalid operator!");
+        
+        
         break;
+          
     }
+  
 
     input.close();
+  
   }
 }
+
